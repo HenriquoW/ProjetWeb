7f27ec68f0b702e7ebb33dc217f8be4ce3c3945f
12Morgana <morgane_lucy@hotmail.fr>
2016-04-30 18:43:12 +0200

    Page profil à tester + script pour la page
    page modifier_profil.php
    Pour Adrien, sans les commentaires pour tester

--- PHP/Pages/modifier_profil.php
+++ PHP/Pages/modifier_profil.php
@@ -1,17 +1,6 @@
-<form action="modifier_profil.php" method="post">
 	<p> <!-- Paragraphe contenant l'intégralité du formulaire -->
-		<?php
-		#uniquement pour tester avant la mise en commun 
-		class Competiteur{ 
-		private $DDN; 
-		public function get_DDN() { return $this->DDN; } 
-		public function set_DDN($ddn) { $this->DDN = $ddn; }
-		} 
-		$UtilisateurEnCours = new Competiteur(); 
-		$UtilisateurEnCours->set_DDN('12/04/2005');
-		//$UtilisateurEnCours = $_SESSION['UtilisateurCourant'];
-		#uniquement pour tester avant la mise en commun
-
+		<?php 
+    $UtilisateurEnCours = $_SESSION['UtilisateurCourant'];
 		$ClasseUtilisateur = get_class($UtilisateurEnCours);
 		?>
 		<!-- Sur la gauche de l'écran sous l image actuelle du  profil -->
@@ -20,22 +9,12 @@
 			<?php
 				if ($ClasseUtilisateur == 'Competiteur')
         {
-          # echo entier à supprimer
-				  echo '
-				    <img src="fee.jpeg" alt="probleme affichage"/>
-				    </br>
-				    <label> Photo </label>
-				    <input name="photo" type="file"/></br>
-				  ';
-          #à décommenter pour tester avec 'vrai' utilisateur
-          /*
           echo '
             <img src="$UtilisateurEnCours->getPhoto()" alt="probleme affichage"/>
 				    </br>
 				    <label> Photo </label>
 				    <input name="photo" type="file"/></br>
 				  ';
-          */
         }
 			?>
 		</p>
@@ -45,33 +24,23 @@
 		<!-- /!\ Vérification si mineur -->
 		<p>
 		    <?php
-          #$DDNUser = date_parse($UtilisateurEnCours->getDateNaissance());
-			    $DDNUser = date_parse($UtilisateurEnCours->get_DDN()); # à supprimer
+          $DDNUser = date_parse($UtilisateurEnCours->getDateNaissance());
 			    $YearUser = $DDNUser['year'];
 			    $YearToday = getdate()['year'];
 			    if( $YearToday - $YearUser < '18')
           {
-          #echo entier à supprimer
-			      echo '
-			        <label> Nom </label>
-			        <input name="nom_responsable" type="text" disabled/> </br>
-			        <label> Prenom </label>
-			        <input name="prenom_responsable" type="text" disabled/> </br>
-			        <label> Coordonnees </label>
-			        <textarea name="coordonnees_responsable" type="text" disabled> </textarea>
-		    	      ';
-            /*
+            $TableauResultat=$UtilisateurEnCours->getParente();
+            $TableauParents=$TableauResultat['Parent'];
+            $idParent=$TableauParents[0];
+            $Parent=loadUtilisateur($idParent);
             echo '
 			        <label> Nom </label>
-              ############################TO COMPLETE ##################################
-              #getParent retourne un tableau avec l id des parents puis je pourrais utiliser la fonction load(avec l id en paramètre) pour créer un objet avce les infos du parent dedans
-              <input name=" " type="text" disabled/> </br>
+              <input name="nom_responsable" value="$Parent->getNom()" type="text" disabled/> </br>
 			        <label> Prenom </label>
-              <input name="" type="text" disabled/> </br>
+              <input name="prenom_responsable" value="$Parent->getPrenom()" type="text" disabled/> </br>
 			        <label> Coordonnees </label>
-              <textarea name="" type="text" disabled> </textarea>
+              <textarea name="coordonnees_responsable" value="$Parent->getTelephone $Parent->getAdresse()" type="text" disabled> </textarea>
 		    	      ';
-            */
               }
 		    ?>
 		</p>
@@ -79,22 +48,11 @@
 
 		<p> <!-- Centré avec les valeurs actuelles des champs pré-rempli (utiliser value="")-->
 			<label> Nom :</label>
-      <!-- <input name="$UtilisateurEnCours->getNom()" type="text" /> <br/>-->
-			<input name="nom" type="text" /> <br/> <!--à supprimer-->
+      <input name="$UtilisateurEnCours->getNom()" type="text" /> <br/>
 			<label> Prenom :</label>
-      <!-- <input name="$UtilisateurEnCours->getPrenom()" type="text" /> <br/> -->
-      
-      <!--à supprimer-->
-			<input name="prenom" type="text" /> <br/>
-			<label> Sexe :</label> <br/>
-      <label>F</label> 
-			<input name="sexe" type="radio" value="F" />
-			<label>M</label>
-			<input name="sexe" type="radio" value="M"/>  <br/>
-      <!--jusqu'ici-->  
+      <input name="$UtilisateurEnCours->getPrenom()" type="text" /> <br/>  
       
       <?php
-      /*
         $Sexe=$UtilisateurEnCours->getSexe()[type];
         if($Sexe == 'F')
           {
@@ -114,22 +72,12 @@
 			       <input name="sexe" type="radio" value="M" checked="checked"/>  <br/>
             ';
           }
-      */
       ?>
       
-			<!-- Avec un calendrier ?? (a bit long) -->
 			<label>Date de naissance :</label> <br/>
 			<label> Jour </label>
-      <!-- à supprimer -->
-      <input name="jour" type="number" min="1" max="31"/>
-			<label> Mois </label>
-			<input name="mois" type="number" min="1" max="12"/>
-			<label> Annee </label>
-			<input name="annee" type="number" min="1920" max="2016"/> <br/>
-      <!-- jusqu'ici-->
       
       <?php
-        /*
         $DDNUser = date_parse($UtilisateurEnCours->getDateNaissance());
 			  $YearUser = $DDNUser['year'];
 			  $MonthUser = $DDNUser['month'];
@@ -141,63 +89,38 @@
 			    <label> Annee </label>
 			    <input name="annee" type="number" min="1920" max="2016" value="$YearUser"/> <br/>
         ';
-        */
       ?>
       
 			<!-- /!\ Vérification si compétiteur -->
 			<?php
 				if ($ClasseUtilisateur == 'Competiteur')
         {
-          #echo entier à supprimer
-				  echo '
-				    <label> Categorie </label>
-				    <input name="categorie" type="text" disabled/>
-				  ';
-          /*
           echo '
 				    <label> Categorie </label>
             <input name="categorie" type="text" disabled value="$UtilisateurEnCours->getCategorie()"/>
 				  ';
-          */
         }
 		  ?>
 			<!-- /!\ -->
       
 			<br/>
 			<label> Mot de passe</label>
-      <!--<input name="mdp" type="text" value="$UtilisateurEnCours->getPassword()" /> <br/>-->
-			<input name="mdp" type="text" /> <br/> <!--à supprimer-->
+      <input name="mdp" type="text" value="$UtilisateurEnCours->getPassword()" /> <br/>
 			<label> Email </label> 
-      <!--<input name="email" type="email" value="$UtilisateurEnCours->getMail()"/> <br/>-->
-			<input name="email" type="email"/> <br/> <!--à supprimer-->
+      <input name="email" type="email" value="$UtilisateurEnCours->getMail()"/> <br/>
 			<label> Telephone </label>
-      <!--<input name="telephone" type="tel" pattern="[0-9]{10}" value="$UtilisateurEnCours->getTelephone()"/> <br/>-->
-			<input name="telephone" type="tel" pattern="[0-9]{10}"/> <br/> <!--à supprimer-->
+      <input name="telephone" type="tel" pattern="[0-9]{10}" value="$UtilisateurEnCours->getTelephone()"/> <br/>
 			<!-- taille plus grande fixée en css (width et height) ?? (au lieu de rows & cols) && comment faire en sorter que l'utilisateur ne puisse l'agrandir avec les petites flèches ??-->
 			<label>Adresse complete :</label> <br/>
-      <!--<textarea name="adresse" rows="7" cols="50" value="$UtilisateurEnCours->getAdresse()"></textarea> <br/>	-->
-			<textarea name="adresse" rows="7" cols="50"></textarea> <br/>	 <!--à supprimer-->
+      <textarea name="adresse" rows="7" cols="50" value="$UtilisateurEnCours->getAdresse()"></textarea> <br/>
 			<br/>
 			<label> N° Licence </label>
-      <!--<input name="num_licence" type="number" disabled="disabled" value="$UtilisateurEnCours->getNumeroLicence()"/> <br/>-->
-			<input name="num_licence" type="number" disabled="disabled" /> <br/><!--à supprimer-->
+      <input name="num_licence" type="number" disabled="disabled" value="$UtilisateurEnCours->getNumeroLicence()"/> <br/>
       
 			<!--/!\ Vérification si compétiteur -->
 			<?php
 				if ($ClasseUtilisateur == 'Competiteur')
         {
-        #echo entier à supprimer
-				  echo '
-				    <label> Specialite </label> </br>
-				    <label> Kayak </label>
-				    <input name="specialite" type="radio" value="Kayak" />
-				    <label> Canoe </label>
-				    <input name="specialite" type="radio" value="Canoe"/> </br>		 
-				    <label> Objectif(s) :</label> </br>
-				    <textarea name="objectif" type="text" rows="3" cols="20"> </textarea> </br>
-				  ';
-          
-          /*
            echo '
 				    <label> Specialite </label> </br>
 				    <label> Kayak </label>
@@ -223,33 +146,55 @@
 				      <label> Objectif(s) :</label> </br>
 				      <textarea name="objectif" type="text" rows="3" cols="20" value="$UtilisateurEnCours->getObjectif()"> </textarea> </br>
 				    ';
-          */
         }
-		    	?>
+		    ?>
 			<!--/!\ -->
 		</p>
-		<!--En haut à gauche de la page, dans un cadre -->
+		<!--CSS En haut à gauche de la page, dans un cadre -->
 		<p>
 			<!--/!\ Vérification si compétiteur -->
 			<?php
 				if ($ClasseUtilisateur == 'Competiteur')
         {
-				  echo '
-				    <label> Dernier resultat </label> </br>
-				    <input name="dernier_resultat" input="text" disabled/> </br>
-				    <label> Avant-dernier </label> </br>
-				    <input name="avant_dernier_resultat" input="text" disabled /> </br>
-				    <label> Avant avant-dernier</label> </br>
-				    <input name="avant_avant_dernier_resultat" input="text" disabled/> </br>
-				  ';
+          $idPalmaresTableau=$UtilisateurEnCours->getPalmares();
+          $nbID = count($idPalmaresTableau);
+          if($nbID>=1)
+          {
+            $idDernierPalmares=$idPalmaresTableau[$nbID-1];
+            $DernierPalmares= loadPalmares($idDernierPalmares);
+            echo '
+				      <label> Dernier resultat </label> </br>
+				      <input name="dernier_resultat" input="text" value="$DernierPalmares->getClassement()" disabled/> </br>
+              ';
+            $nbID=$nbID-1;  
+          }
+          if($nbID>=1)
+          {
+            $idAvantDernierPalmares=$idPalmaresTableau[$nbID-1];
+            $AvantDernierPalmares=loadPalmares($idAvantDernierPalmares);
+            echo '
+				      <label> Avant-dernier </label> </br>
+				      <input name="avant_dernier_resultat" input="text" value="$AvantDernierPalmares->getClassement()" disabled /> </br>
+              ';
+            $nbID=$nbID-1;  
+          }
+          if($nbID>=1)
+          {
+            $idAvantAvantDernierPalmares=$idPalmaresTableau[$nbID-1];
+            $AvantAvantDernierPalmares=loadPalmares($idAvantAvantDernierPalmares);
+				    echo '
+				      <label> Avant avant-dernier</label> </br>
+				      <input name="avant_avant_dernier_resultat" input="text" value="$AvantAvantDernierPalmares->getClassement()" disabled/> </br>
+				    ';
+          }
         }
 		    	?>
 			<!--/!\ -->		
 		</p>
-		<!-- Centré en bas de la page après tous les champs-->
-		<input type="submit" value="Sauvegarder les modifications"/>
+		<!-- CSS Centré en bas de la page après tous les champs-->
+		<input type="submit" id="Bouton_Sauvegarder" value="Sauvegarder les modifications"/>
+    <!--Appeler script_modifier_profil.php-->
 	</p>
-</form>
 
 
 
---
+++ PHP/Pages/script_modifier_profil.php
@@ -0,0 +1,3 @@
+<?php
+	$UtilisateurEnCours->save(true);
+?>
